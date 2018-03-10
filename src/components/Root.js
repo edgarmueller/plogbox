1e7d87d01b74752deab239092646fc2285d78a3c
Edgar Mueller <emueller@eclipsesource.com>
2018-03-10 22:57:01 +0100

    Update eslint, clean up Root

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import PropTypes from 'prop-types';
 import { ConnectedRouter } from 'react-router-redux';
 import { Route } from 'react-router';
 import App from './App';
 
-// TODO
 import { history } from '../store/configureStore';
 
-const Root = ({ store }) => {
-  const theme = createMuiTheme();
-  return (
-    <MuiThemeProvider theme={theme}>
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Route path="/" component={App} />
-        </ConnectedRouter>
-      </Provider>
-    </MuiThemeProvider>
-  );
-};
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Route path="/" component={App} />
+    </ConnectedRouter>
+  </Provider>
+);
 
 Root.propTypes = {
   store: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
