620964f53ad1bb0a46ba33e0f6add33261502e58
Edgar Mueller <emueller@eclipsesource.com>
2018-03-12 14:11:04 +0100

    Remove LogbookPage.spec and update Editor.spec

--- test/components/Editor.spec.js
+++ test/components/Editor.spec.js
@@ -1,25 +1,14 @@
 /* eslint-disable import/first */
-import { mountWithContext } from '../helpers/setup';
 import React from 'react';
-import { Provider } from 'react-redux';
-import { shallow, mount } from 'enzyme';
-import { MemoryRouter, Switch } from 'react-router';
-import configureMockStore from 'redux-mock-store';
-import { Grid } from 'material-ui'
-
-import { App } from '../../src/components/App';
-import { Editor } from '../../src/components/Editor';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Grid } from 'material-ui';
 import { firstPost } from '../helpers/posts';
-import { RenderedBlock } from '../../src/components/RenderedBlock';
-import withDragDropContext from '../../src/common/withDragDropContext';
+import { Editor } from '../../src/components/Editor';
 
-const mockStore = configureMockStore();
+Enzyme.configure({ adapter: new Adapter() });
 
 test('Render Editor', () => {
-    console.log('first post ', firstPost)
-     const Component = Editor;
-    const wrapper = shallow(
-        <Component post={firstPost} />
-    );
-    expect(wrapper.find(Grid).length).toBe(4);
+  const wrapper = shallow(<Editor post={firstPost} />);
+  expect(wrapper.find(Grid).length).toBe(3);
 });
