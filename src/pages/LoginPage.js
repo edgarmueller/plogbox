e4ee96e35327a1de179b5c2225ade08d676bcc3c
Edgar Mueller <emueller@eclipsesource.com>
2018-03-13 23:24:27 +0100

    Update styling of login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,39 +3,89 @@ import * as _ from 'lodash';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
-import { Field, reduxForm } from 'redux-form';
 import PropTypes from 'prop-types';
 import { routerActions } from 'react-router-redux';
-import { Button, FormLabel } from 'material-ui';
+import { Button } from 'material-ui';
 import red from 'material-ui/colors/red';
 import { TextField } from 'redux-form-material-ui';
+import Radium from 'radium';
 
 import { loginUser } from '../actions/index';
-import { renderPasswordTextField } from '../utils/helpers';
 import '../common/tap';
 import { getStatusText } from '../reducers/auth';
 
-const form = reduxForm({
-  form: 'login',
-});
+const buttonStyle = {
+  textDecoration: 'none',
+  color: '#333435',
+  paddingLeft: '2em',
+  paddingRight: '2em',
+  borderRadius: '28px',
+  border: '2px solid #80CBC4',
+  height: '28px',
+  display: 'inline-flex',
+  alignItems: 'center',
+  marginLeft: '1em',
+  marginRight: '1em',
+  fontWeight: 'bold',
+  '&:hover': {
+    backgroundColor: '#80CBC4',
+    color: '#fff',
+  },
+  textTransform: 'uppercase',
+  fontSize: '10px',
+  letterSpacing: '0.065em',
+};
 
-export const LoginForm = ({ handleSubmit, handleFormSubmit, renderAlert }) => (
-  <form onSubmit={handleSubmit(handleFormSubmit)}>
-    <FormLabel component="legend">Mail</FormLabel>
-    <Field name="email" component={TextField} />
+const forgotPasswordStyle = {
+  textDecoration: 'none',
+  color: '#333435',
+  paddingRight: '2em',
+  height: '28px',
+  display: 'inline-flex',
+  marginLeft: '1em',
+  marginRight: '1em',
+  fontWeight: 'bold',
+  '&:hover': {
+    backgroundColor: '#80CBC4',
+    color: '#fff',
+  },
+  alignItems: 'center',
+  textTransform: 'uppercase',
+  fontSize: '0.8em',
+  letterSpacing: '0.065em',
+};
 
-    <FormLabel component="legend">Password</FormLabel>
-    <Field name="password" component={renderPasswordTextField} />
+const RadiumLink = Radium(Link);
 
-    <Button type="submit" color={'primary'}>
-      Login
-    </Button>
+export const LoginForm = ({ handleFormSubmit, renderAlert }) => (
+  <form onSubmit={handleFormSubmit}>
+    <TextField
+      required
+      type="text"
+      label={'Mail address'}
+    />
+    <br />
+    <TextField
+      required
+      type="password"
+      label={'Password'}
+    />
+    <br />
+
+    <p style={{ display: 'flex', marginTop: '1em' }}>
+      <RadiumLink to="/password/forgot" style={forgotPasswordStyle} >
+          Forgot password?
+      </RadiumLink>
+      <Button type="submit" color={'primary'} style={buttonStyle}>
+        Login
+      </Button>
+    </p>
+
     {renderAlert()}
   </form>
 );
 
 LoginForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
   handleFormSubmit: PropTypes.func.isRequired,
   renderAlert: PropTypes.func.isRequired,
 };
@@ -56,7 +106,7 @@ class LoginFormContainer extends React.Component {
     if (this.props.errorMessage) {
       return (
         <div style={{ marginTop: '1em' }}>
-          <span style={{ color: red }}>
+          <span style={{ color: red[400] }}>
             {this.props.errorMessage}
           </span>
         </div>
@@ -83,19 +133,24 @@ class LoginFormContainer extends React.Component {
       return (<p>Logging in...</p>);
     }
     return (
-      <div style={{ marginTop: '1em' }}>
+      <div
+        style={{
+          paddingBottom: '1.5em',
+          paddingLeft: '1em',
+          backgroundColor: '#fff',
+          borderBottomLeftRadius: '0.25em',
+          borderBottomRightRadius: '0.25em',
+          boxShadow: '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
+          width: '40%',
+          margin: 'auto',
+        }}
+      >
+        <h2 style={{ paddingTop: '1em' }}>Welcome to _plog</h2>
         <LoginForm
           handleSubmit={handleSubmit}
           handleFormSubmit={this.handleFormSubmit}
           renderAlert={this.renderAlert}
         />
-        <p>
-          <Link to="/password/forgot">
-            <Button>
-              Forgot password?
-            </Button>
-          </Link>
-        </p>
       </div>
     );
   }
@@ -131,5 +186,5 @@ const mapDispatchToProps = {
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-)(form(LoginFormContainer));
+)(LoginFormContainer);
 
