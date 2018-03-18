263c41799e2891ce49d98484c57711e566f86b39
Edgar Mueller <edgar.m.mueller@gmail.com>
2018-03-18 13:13:29 +0100

    Fix wrong import

--- src/components/RenderedPost.js
+++ src/components/RenderedPost.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import RenderedBlock from './RenderedBlock';
+import RenderedBlock from '../containers/RenderedBlockContainer';
 
 const RenderedPost = ({ post, isFetchingBlock, focusedBlockId }) => (
   <div>
