6d00f527c2f5015b4304a00ed485723a0d7fa6c2
Edgar Mueller <emueller@eclipsesource.com>
2018-09-19 11:03:15 +0200

    Configure common styles

--- src/common/styles.js
+++ src/common/styles.js
@@ -1,3 +1,5 @@
+import red from 'material-ui/colors/red';
+
 export const button = {
   textDecoration: 'none',
   color: '#333435',
@@ -19,13 +21,18 @@ export const button = {
   letterSpacing: '0.065em',
 };
 
+export const formButtonBar = {
+  display: 'flex',
+  marginTop: '1em',
+};
+
 export const appBar = {
   backgroundColor: '#fff',
   borderBottom: '1px solid #ebebeb',
   width: '100%',
   borderBottomLeftRadius: '0.5em',
   borderBottomRightRadius: '0.5em',
-}
+};
 
 export const card = {
   marginTop: '1em',
@@ -34,14 +41,19 @@ export const card = {
   padding: '8px',
   background: '#fff',
   borderRadius: '0.5em',
-  fontSize: '0.85em'
+  fontSize: '0.85em',
 };
 
 export const header = {
-  fontFamily: "'Montserrat', sans-serif", 
+  fontFamily: "'Montserrat', sans-serif",
   color: '#6a6969',
-}
- 
+};
+
 export const cardContent = {
-  padding: '0'
+  padding: '0',
+};
+
+export const errorStyle = {
+  color: red[400],
+  paddingTop: '1em',
 };
