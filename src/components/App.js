0b44e8b19ecc415d8c79313eeebf4dfcb34a85cf
Edgar Mueller <emueller@eclipsesource.com>
2018-10-02 16:57:55 +0200

    Remove navigation bar and post routes from App

--- src/components/App.js
+++ src/components/App.js
@@ -5,9 +5,7 @@ import { MuiThemeProvider } from 'material-ui';
 import { createMuiTheme } from 'material-ui/styles';
 import 'react-dates/initialize';
 
-import Navigation from './NavBar';
-import PostList from '../containers/PostListContainer';
-import EditPostPage from '../pages/EditPostPage';
+import MainPage from '../pages/MainPage';
 import NotFoundPage from '../pages/NotFoundPage';
 import SignUpPage from '../pages/SignUpPage';
 import Login from '../pages/LoginPage';
@@ -16,12 +14,6 @@ import ForgotPasswordPage from '../pages/ForgotPasswordPage';
 import ResetPasswordPage from '../pages/ResetPasswordPage';
 import ActivateAccountPage from '../pages/ActivateAccountPage';
 import { requireAuth } from '../auth';
-
-
-const containerStyle = {
-  width: '70%',
-  margin: 'auto',
-};
 
 const theme = createMuiTheme({
   mixins: {
@@ -37,28 +29,21 @@ const ConnectedSwitch = connect(state => ({
 
 export const App = () => (
   <MuiThemeProvider theme={theme}>
-    <div>
-      <Navigation />
-      <div id="container" style={containerStyle}>
-        <ConnectedSwitch>
-          <Route path="/sign-up" component={SignUpPage} />
-          <Route path="/login" component={Login} />
-          <Route path="/posts/:postId" component={requireAuth(EditPostPage)} />
-          <Route path="/posts" component={requireAuth(PostList)} />
-          <Route path="/profile" component={requireAuth(ProfilePage)} />
-          <Route path="/password/reset/:token" component={ResetPasswordPage} />
-          <Route path="/password/forgot" component={ForgotPasswordPage} />
-          <Route path="/account/activate/:token" component={ActivateAccountPage} />
-          <Route path="/" component={requireAuth(PostList)} />
-          <Route path="*" component={NotFoundPage} />
-        </ConnectedSwitch>
-      </div>
+    <div id="container">
+      <ConnectedSwitch>
+        <Route path="/sign-up" component={SignUpPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/profile" component={requireAuth(ProfilePage)} />
+        <Route path="/password/reset/:token" component={ResetPasswordPage} />
+        <Route path="/password/forgot" component={ForgotPasswordPage} />
+        <Route path="/account/activate/:token" component={ActivateAccountPage} />
+        <Route path="/" component={requireAuth(MainPage)} />
+        <Route path="*" component={NotFoundPage} />
+      </ConnectedSwitch>
     </div>
   </MuiThemeProvider>
 );
 
-const ConnectedApp = connect(
-  state => ({ location: state.routing.location }),
-)(App);
+const ConnectedApp = connect(state => ({ location: state.routing.location }))(App);
 
 export default ConnectedApp;
