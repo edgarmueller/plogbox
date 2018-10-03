5174fde5579e0397f05c2a54c92ecebd214f365c
Edgar Mueller <emueller@eclipsesource.com>
2018-10-03 14:10:18 +0200

    Remove name field from post button bar

--- src/components/EditPostButtonBar.js
+++ src/components/EditPostButtonBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { IconButton, TextField, Tooltip, withStyles } from 'material-ui';
+import { IconButton, Tooltip, withStyles } from 'material-ui';
 import NavigationCheck from 'material-ui-icons/Check';
 import ContentArchive from 'material-ui-icons/Archive';
 import ContentUnarchive from 'material-ui-icons/Unarchive';
@@ -26,119 +26,76 @@ const styles = () => ({
 
 const EditPostButtonBar =
   ({
-     post,
-     classes,
-     exportPost,
-     importPost,
-     savePost,
-     upload,
-     showTitle,
-     handleUpdatePost,
-     navigateToPosts,
-     handleClickOpenEditor,
-     handleClickOpenRenderedView,
-     handleClickOpenBoth,
-   }) =>
-    (
-      <span>
-        {
-          showTitle &&
-          <TextField
-            name="title"
-            type="text"
-            placeholder="Enter post title"
-            value={post.title}
-            onChange={ev => handleUpdatePost({
-              ...post,
-              title: ev.target.value,
-            })}
-            InputLabelProps={{
-              shrink: true,
-              className: classes.textFieldFormLabel,
-            }}
-            InputProps={{
-              classes: {
-                input: classes.textFieldInput,
-              },
-            }}
-          />
-        }
-        <Tooltip title="Save this post">
-          <IconButton
-            onClick={() => savePost(post)
-              .then(
-                (updatedPost) => {
-                  handleUpdatePost(updatedPost);
-                },
-                // TODO: error handling
-                error => console.error('TODO: error occurred', error),
-              )
-            }
-          >
-            <ContentSave />
-          </IconButton>
-        </Tooltip>
+    post,
+    classes,
+    exportPost,
+    importPost,
+    savePost,
+    upload,
+    handleClickOpenEditor,
+    handleClickOpenRenderedView,
+    handleClickOpenBoth,
+  }) => (
+    <span>
+      <Tooltip title="Save this post">
+        <IconButton
+          style={{ color: '#333435' }}
+          onClick={() => savePost(post)}
+        >
+          <ContentSave />
+        </IconButton>
+      </Tooltip>
 
-        <Tooltip title="Save and go back to post list">
-          <IconButton
-            onClick={
-              () => savePost(post)
-                .then(
-                  (updatedPost) => {
-                    handleUpdatePost(updatedPost);
-                    navigateToPosts();
-                  },
-                  // TODO: error handling
-                  error => console.error('TODO: error occurred', error),
-                )
-            }
-          >
-            <NavigationCheck />
-          </IconButton>
-        </Tooltip>
+      <Tooltip title="Save and go back to post list">
+        <IconButton
+          onClick={() => savePost(post)}
+        >
+          <NavigationCheck />
+        </IconButton>
+      </Tooltip>
 
-        <Tooltip title="Export this post as a JSON file">
-          <IconButton onClick={() => exportPost(post)}>
-            <ContentArchive />
-          </IconButton>
-        </Tooltip>
+      <Tooltip title="Export this post as a JSON file">
+        <IconButton onClick={() => exportPost(post)}>
+          <ContentArchive />
+        </IconButton>
+      </Tooltip>
 
-        <Tooltip title="Import the blocks of an exported post">
-          <IconButton onClick={() => importPost()}>
-            <ContentUnarchive />
-          </IconButton>
-        </Tooltip>
+      <Tooltip title="Import the blocks of an exported post">
+        <IconButton onClick={() => importPost()}>
+          <ContentUnarchive />
+        </IconButton>
+      </Tooltip>
 
-        <Tooltip title={'Toggle side-by-side view'}>
-          <IconButton onClick={handleClickOpenBoth} className={classes.sideBySideButton}>
-            <SideBySideIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title={'Show Editor'}>
-          <IconButton onClick={handleClickOpenEditor} className={classes.editorButton}>
-            <EditorIcon />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title={'Show Rendered Document'}>
-          <IconButton onClick={handleClickOpenRenderedView} className={classes.viewButton}>
-            <RenderedDocumentIcon />
-          </IconButton>
-        </Tooltip>
+      <Tooltip title="Toggle side-by-side view">
+        <IconButton onClick={handleClickOpenBoth} className={classes.sideBySideButton}>
+          <SideBySideIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Show Editor">
+        <IconButton onClick={handleClickOpenEditor} className={classes.editorButton}>
+          <EditorIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Show Rendered Document">
+        <IconButton onClick={handleClickOpenRenderedView} className={classes.viewButton}>
+          <RenderedDocumentIcon />
+        </IconButton>
+      </Tooltip>
 
-        <input
-          id={'upload'}
-          type="file"
-          className={classes.hidden}
-          onChange={event => upload(post, event.target.files[0])}
-        />
-      </span>
-    );
+      <input
+        id="upload"
+        type="file"
+        className={classes.hidden}
+        onChange={event => upload(post, event.target.files[0])}
+      />
+    </span>
+  );
 
 EditPostButtonBar.propTypes = {
   classes: PropTypes.shape({
     textFieldFormLabel: PropTypes.string,
   }).isRequired,
-  // TODO: duplciate prop type definition
+  // TODO: duplicate prop type definition
   post: PropTypes.shape({
     title: PropTypes.string,
     blocks: PropTypes.arrayOf(PropTypes.object),
@@ -147,21 +104,9 @@ EditPostButtonBar.propTypes = {
   importPost: PropTypes.func.isRequired,
   savePost: PropTypes.func.isRequired,
   upload: PropTypes.func.isRequired,
-  showTitle: PropTypes.bool,
-
-  navigateToPosts: PropTypes.func.isRequired,
-  handleUpdatePost: PropTypes.func.isRequired,
   handleClickOpenEditor: PropTypes.func.isRequired,
   handleClickOpenRenderedView: PropTypes.func.isRequired,
   handleClickOpenBoth: PropTypes.func.isRequired,
-};
-
-EditPostButtonBar.defaultProps = {
-  blocks: [],
-  showTitle: false,
-  showBoth: true,
-  showRenderedView: false,
-  showEditor: false,
 };
 
 export default withStyles(styles)(EditPostButtonBar);
