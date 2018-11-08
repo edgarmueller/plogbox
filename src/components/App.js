71dc767fa2713282d4cfa65553a4ccf6825f2374
Edgar Mueller <emueller@eclipsesource.com>
2018-11-08 10:22:56 +0100

    Remove obsolete pages/components/routes

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,6 @@ import { MuiThemeProvider } from 'material-ui';
 import { createMuiTheme } from 'material-ui/styles';
 import 'react-dates/initialize';
 import NotFoundPage from '../pages/NotFoundPage';
-import SignUpPage from '../pages/SignUpPage';
-import ProfilePage from '../pages/ProfilePage';
-import ForgotPasswordPage from '../pages/ForgotPasswordPage';
-import ActivateAccountPage from '../pages/ActivateAccountPage';
-import { requireAuth } from '../auth';
 import DropboxReceiver from './DropboxReceiver';
 import MainPage from '../pages/MainPage';
 
@@ -29,10 +24,6 @@ export const App = () => (
   <MuiThemeProvider theme={theme}>
     <div id="container">
       <ConnectedSwitch>
-        <Route path="/sign-up" component={SignUpPage} />
-        <Route path="/profile" component={requireAuth(ProfilePage)} />
-        <Route path="/password/forgot" component={ForgotPasswordPage} />
-        <Route path="/account/activate/:token" component={ActivateAccountPage} />
         <Route
           exact
           path="/auth"
--- src/pages/ResetPasswordPage.js
+++
@@ -1,7 +0,0 @@
-import { connect } from 'react-redux';
-import {
-  ResetPasswordFormContainer,
-  mapStateToProps,
-} from '../containers/ResetPasswordFormContainer';
-
-export default connect(mapStateToProps)(ResetPasswordFormContainer);
