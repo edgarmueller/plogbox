709aa5375d7dc5482e070566c48266e92b644402
Edgar Mueller <emueller@eclipsesource.com>
2018-11-13 20:07:42 +0100

    Update styling for RenderedView, show logo for empty files

--- src/components/RenderedView.js
+++ src/components/RenderedView.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import _ from 'lodash';
 import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui';
+import newFileIcon from './newfile.svg';
 
 const md = require('markdown-it')();
 const mk = require('markdown-it-katex');
@@ -21,15 +23,45 @@ const styles = () => ({
     lineHeight: '1.5em',
     backgroundColor: '#FEFAD7',
     color: '#4c4a37',
-    border: '1px solid rgba(0, 0, 0, 0.12)'
+    border: '1px solid rgba(0, 0, 0, 0.12)',
+    height: '100%'
+  },
+  center: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    height: '100%'
+  },
+  icon: {
+    fontSize: '2em',
+    flexGrow: 0.5,
+    paddingTop: '1em'
   }
 });
 
-const RenderedView = ({ classes, text }) => (
-  <div className={classes.view}>
-    <ReactMarkdown source={md.render(text)} escapeHtml={false} />
-  </div>
-);
+const RenderedView = ({ classes, text }) => {
+  if (_.isEmpty(text)) {
+    return (
+      <div className={classes.center}>
+        <img
+          src={newFileIcon}
+          alt="logo"
+          height="100"
+        />
+        <p className={classes.icon}>
+          Empty post. Start working on it now :)
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.view}>
+      <ReactMarkdown source={md.render(text)} escapeHtml={false} />
+    </div>
+  );
+};
 
 RenderedView.propTypes = {
   classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
