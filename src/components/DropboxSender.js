d542ef9c1f0693363d3c873ee75751a541d36803
Edgar Mueller <emueller@eclipsesource.com>
2018-11-27 22:38:00 +0100

    Extract redirect URI to env var

--- src/components/DropboxSender.js
+++ src/components/DropboxSender.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { OauthSender } from 'react-oauth-flow';
 
-const DropboxSender = () => {
-  console.log('>>', process.env.REACT_APP_CLIENT_ID)
-  return (
-    <OauthSender
-      authorizeUrl="https://www.dropbox.com/oauth2/authorize"
-      clientId={process.env.REACT_APP_CLIENT_ID}
-      redirectUri="http://localhost:3000/auth"
-      render={({ url }) => <a href={url}>Connect to Dropbox</a>}
-    />
-  );
-}
+const DropboxSender = () => (
+  <OauthSender
+    authorizeUrl="https://www.dropbox.com/oauth2/authorize"
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    redirectUri={process.env.REACT_APP_REDIRECT_URI}
+    render={({ url }) => <a href={url}>Connect to Dropbox</a>}
+  />
+);
 
 export default DropboxSender;
