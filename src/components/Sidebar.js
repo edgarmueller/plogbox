0b92abfcb62c0c4d9ba3a6804656a12085162e94
Edgar Mueller <edgar.m.mueller@gmail.com>
2018-12-01 20:17:47 +0100

    Remove padding from Drawer

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,37 +1,37 @@
-import React from 'react';
-import _ from 'lodash';
-import PropTypes from 'prop-types';
-import withStyles from '@material-ui/core/styles/withStyles';
-import Drawer from '@material-ui/core/Drawer';
-import Radium from 'radium';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import TagList from '../containers/TagListContainer';
-import UserIndicator from './UserIndicator';
-import DropboxSender from './DropboxSender';
+import React from "react";
+import _ from "lodash";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Drawer from "@material-ui/core/Drawer";
+import Radium from "radium";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import TagList from "../containers/TagListContainer";
+import UserIndicator from "./UserIndicator";
+import DropboxSender from "./DropboxSender";
 
 const RadiumLink = Radium(Link);
 const drawerWidth = 240;
 const styles = theme => ({
   logo: {
-    color: '#333435',
-    borderRadius: '16px',
-    paddingLeft: '2em',
-    paddingRight: '2em',
-    paddingTop: '1em',
-    paddingBottom: '1em',
-    fontSize: '1.8125em',
-    fontWeight: 'bold',
-    textDecoration: 'none',
-    fontFamily: "'Montserrat', sans-serif",
+    color: "#333435",
+    borderRadius: "16px",
+    paddingLeft: "2em",
+    paddingRight: "2em",
+    paddingTop: "1em",
+    paddingBottom: "1em",
+    fontSize: "1.8125em",
+    fontWeight: "bold",
+    textDecoration: "none",
+    fontFamily: "'Montserrat', sans-serif"
   },
   drawerPaper: {
+    border: "none",
     width: drawerWidth,
-    padding: 10,
-    [theme.breakpoints.up('md')]: {
-      position: 'relative',
-    },
-  },
+    [theme.breakpoints.up("md")]: {
+      position: "relative"
+    }
+  }
 });
 
 const Sidebar = ({ classes, user }) => (
@@ -39,30 +39,28 @@ const Sidebar = ({ classes, user }) => (
     variant="permanent"
     open
     classes={{
-        paper: classes.drawerPaper,
-      }}
+      paper: classes.drawerPaper
+    }}
   >
     <RadiumLink className={classes.logo} to="/">
-        plog_
+      plog_
     </RadiumLink>
-    {
-        user ?
-          (
-            <React.Fragment>
-              <UserIndicator />
-              <TagList />
-            </React.Fragment>
-          ) :
-            <DropboxSender
-              render={({ url }) => <a href={url}>Connect to Dropbox</a>}
-            />
-      }
+    {user ? (
+      <React.Fragment>
+        <UserIndicator />
+        <TagList />
+      </React.Fragment>
+    ) : (
+      <DropboxSender
+        render={({ url }) => <a href={url}>Connect to Dropbox</a>}
+      />
+    )}
   </Drawer>
 );
 
 Sidebar.propTypes = {
   user: PropTypes.string,
-  classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  classes: PropTypes.object.isRequired // eslint-disable-line react/forbid-prop-types
 };
 
 Sidebar.defaultProps = {
@@ -70,7 +68,7 @@ Sidebar.defaultProps = {
 };
 
 const mapStateToProps = state => ({
-  user: _.get(state, 'auth.user.name.display_name'),
+  user: _.get(state, "auth.user.name.display_name")
 });
 
 export default withStyles(styles)(connect(mapStateToProps)(Sidebar));
